42eebe1eb1989365126f2ba0847f7b7e56beb056
Your Name <your.email@example.com>
2025-11-26 23:15:41 +0700

    complete add to cart service and controller

---
+++ src/controllers/cart-controller.ts
@@ -0,0 +1,41 @@
+import { Request, Response } from "express";
+import { handleSuccess, handleError } from "@/utils/response-util";
+import { handleControllerError } from "@/utils/helper/controller-error-handler";
+import { addToCartService, removeFromCartService, getCartService } from "@/services/cart-service";
+
+// get cart
+export const getCartController = async (req: Request, res: Response) => {
+    try {
+        // Runtime check if user exist if exist it continue to the next step if not it return 401 error
+        if (!req.user) {
+          return handleError(res, 401, "Unauthorized");
+        };
+
+        const userId = req.user._id;
+        const carts = await getCartService(userId);
+
+        return handleSuccess(res, 200, "Get cart successfully", carts);
+    } catch (error) {
+        handleControllerError(res, error)
+        // next(error);
+    }
+}
+
+// add to cart
+export const addToCartController = async (req: Request, res: Response) => {
+    try {
+        // Runtime check if user exist if exist it continue to the next step if not it return 401 error
+        if (!req.user) {
+          return handleError(res, 401, "Unauthorized");
+        };
+
+        const userId = req.user._id;
+        const { productId, quantity } = req.body;
+
+        const cart = await addToCartService(userId, productId, quantity || 1);
+
+        return handleSuccess(res, 200, "Add to cart successfully", cart);
+    } catch (error) {
+        handleControllerError(res, error);
+    }
+}
